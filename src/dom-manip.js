767b60e23de311d577857e9247fb01dbfb4c6000
root <root@FRTOU-LT-1384197.localdomain>
2023-12-28 00:53:27 +0100

    adding functions to dom-manip.js file

--- src/dom-manip.js
+++ src/dom-manip.js
@@ -44,10 +44,83 @@ export const dom = (() => {
           mainContent.classList.remove('inactive-main');
         }
       }
+    
+      function toggleMenu() {
+        toggleMenuIcon.classList.toggle('active');
+    
+        // Show Sidebar and make main content a bit transparent
+        if (sidebarMenu.classList.contains('hide-sidebar')) {
+          sidebarMenu.classList.remove('hide-sidebar');
+          sidebarMenu.classList.add('show-sidebar');
+          mainContent.classList.add('inactive-main');
+    
+          // Hide Sibar and make main content opaque 
+        } else if (sidebarMenu.classList.contains('show-sidebar')) {
+          sidebarMenu.classList.remove('show-sidebar');
+          sidebarMenu.classList.add('hide-sidebar');
+          mainContent.classList.remove('inactive-main');
+        }
+      }
+
+    // Main content title
+    function showMainTitle(index) {
+      const allMenuIcons = document.querySelectorAll('.menu-link-icon');
+      const menuIcon = allMenuIcons[index].getAttribute('data-icon');
+      const menuTexts = document.querySelectorAll('.menu-link-text');
+
+      mainTitleIcon.classList.add(
+        'fal',
+        'fa-fw',
+        'main-title-icon',
+        'padding-right',
+        menuIcon
+      );
+      mainTitleText.textContent = menuTexts[index].textContent;
+      document.title = `ToDo - ${mainTitleText.textContent}`;
+    }
+
+    function changeMainTitle(target, index) {
+      mainTitleIcon.className = '';
+  
+      // Title of tasks from the menu 
+      if (
+        target.classList.contains('menu-link') ||
+        target.classList.contains('menu-link-icon') ||
+        target.classList.contains('menu-link-text')
+      ) {
+        showMainTitle(index);
+      }
+  
+      // Title of tasks from projects
+      if (
+        target.classList.contains('project-link') ||
+        target.classList.contains('project-icon') ||
+        target.classList.contains('project-text') ||
+        target.classList.contains('delete-project') ||
+        target.classList.contains('edit-project') ||
+        target.classList.contains('project-icon-and-text-div') ||
+        target.classList.contains('project-default-icons-div')
+      ) {
+        const projectIcon = projects.projectsList[index].icon;
+  
+        mainTitleIcon.classList.add(
+          'fal',
+          'fa-fw',
+          'main-title-icon',
+          'padding-right',
+          projectIcon
+        );
+        mainTitleText.textContent = projects.projectsList[index].title;
+        document.title = `ToDo - ${mainTitleText.textContent}`;
+      }
+    }
 
 
       return {
         responsiveMenu,
+        toggleMenu,
+        showMainTitle,
+        changeMainTitle,
       };
 
 })();
