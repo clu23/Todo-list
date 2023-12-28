deebb364bb4448c9fca8effbcaf412ead0aa7993
root <root@FRTOU-LT-1384197.localdomain>
2023-12-28 22:11:03 +0100

    adding showTasks function to dom manip js file

--- src/dom-manip.js
+++ src/dom-manip.js
@@ -188,22 +188,22 @@ export const dom = (() => {
         modalIconsDiv.classList.add('show');
         modalTasksDiv.classList.add('hide');
   
-        // IF MODAL IS FOR EDITING PROJECT
+        // If modal is for editing a project 
         if (modalTask === 'Edit Project') {
           const allProjectIcons = modal.querySelectorAll('.icon');
           const projectIcon = projects.projectsList[projectIndex].icon;
   
-          // SHOW EDITABLE PROJECT TITLE
+          // Show editable project title 
           modalTitle.value = projects.projectsList[projectIndex].title;
   
-          // SELECT EDITABLE PROJECT ICON
+          // Select editable project icon 
           for (let i = 0; i < allProjectIcons.length; i += 1) {
             if (allProjectIcons[i].value === projectIcon) {
               allProjectIcons[i].checked = true;
             }
           }
   
-        // IF MODAL IS FOR ADDING OR EDITING TASK
+        // If modal is for adding or editing a task 
         } else if (modalTask === 'Add Task'||
             modalTask === 'Edit Task'
         ) {
@@ -218,7 +218,7 @@ export const dom = (() => {
             taskPrioritySelection.value = projects.projectsList[projectIndex].tasks[taskIndex].priority;
           }
   
-          // IF MODAL IS FOR WATCHING TASK INFO
+          // If modal is to display task info 
         } else if (modalTask === 'Task Info') {
           form.classList.add('hide');
           confirmButton.classList.add('hide');
@@ -227,21 +227,21 @@ export const dom = (() => {
         }
       }
   
-      // DELETION MODAL CONTENT
+      // Deletion of modal content 
       if (modalAction === 'Delete') {
         modalHeader.classList.add('deletion-modal-header');
         form.classList.add('hide');
         cancelButton.classList.add('cancel-deletion');
         confirmButton.classList.add('confirm-deletion');
   
-        // PROJECT DELETION
+        // Project deletion
         if (modalTask === 'Delete Project') {
           const projectDeletionTitle = document.querySelector('.project-deletion-title');
   
           projectDeletionText.classList.remove('hide');
           projectDeletionTitle.textContent = projects.projectsList[projectIndex].title;
   
-          // TASK DELETION
+          // Task deletion
         } else if (modalTask === 'Delete Task') {
           const taskDeletionTitle = document.querySelector('.task-deletion-title');
   
@@ -251,10 +251,162 @@ export const dom = (() => {
         }
       }
   
-      // TO CLOSE THE MODAL
+      // To close the modal window 
       if (modalState === 'close') {
         modal.classList.add('hide');
       }
+    }
+
+
+    // This function displays the tasks on the main window depending on the menu selected on the navbar
+    function showTasks(menuTitle, projectIndexStart, projectIndexEnd) {
+      const todayDate = format(new Date(), 'yyyy-MM-dd');
+      let tasksNumber = 0;
+  
+      tasksCount.textContent = 0;
+      tasksList.textContent = '';
+  
+      // Generate tasks list
+      for (let i = projectIndexStart; i < projectIndexEnd; i += 1) {
+        for (let j = 0; j < projects.projectsList[i].tasks.length; j += 1) {
+          const taskDiv = document.createElement('div');
+          const taskIconAndTextDiv = document.createElement('div');
+          const taskIcon = document.createElement('i');
+          const taskText = document.createElement('p');
+          const taskInfo = document.createElement('div');
+          const taskDate = document.createElement('p');
+          const taskEditIcon = document.createElement('i');
+          const taskTrashIcon = document.createElement('i');
+          const taskInfoIcon = document.createElement('i');
+  
+            // If clicked on Menu Link "Today"
+          if (menuTitle === 'today') {
+  
+            if (projects.projectsList[i].tasks[j].date !== todayDate
+            ) {
+              continue; // If task isn't for today - skip it
+            }
+  
+            // If clicked on Menu link "Week" CLICKED ON MENU LINK 'WEEK'
+          } else if (menuTitle === 'week') {
+            const dateOfToday = parseISO(todayDate);
+            const dateOfTask = parseISO(projects.projectsList[i].tasks[j].date)
+  
+            if (!(differenceInDays(dateOfTask, dateOfToday) <= 7 &&
+               differenceInDays(dateOfTask, dateOfToday) >= 0
+            )) {
+             continue; // If the task isn't due within a week from today - skip it
+            }
+  
+            // If clicked on Menu link "Completed"
+          } else if (menuTitle === 'completed' &&
+            projects.projectsList[i].tasks[j].completed !== true
+          ) {
+            continue; // If task isn't completed yet - skip it
+          }
+  
+          // Show number of tasks
+          tasksNumber += 1;
+          tasksCount.textContent = tasksNumber;
+  
+          // Task priority, text and its Div 
+          taskDiv.classList.add('task-div', 'hover-element');
+          taskIconAndTextDiv.classList.add('flex');
+          taskDiv.setAttribute('data-project-index', i);
+          taskDiv.setAttribute('data-task-index', j);
+  
+          if (projects.projectsList[i].tasks[j].priority === 'low') {
+            taskIcon.classList.add('low-priority');
+          } else if (projects.projectsList[i].tasks[j].priority === 'medium') {
+            taskIcon.classList.add('mid-priority');
+          } else if (projects.projectsList[i].tasks[j].priority === 'high') {
+            taskIcon.classList.add('high-priority');
+          } else {
+            taskIcon.classList.add('fal', 'padding-right');
+          }
+          taskIcon.setAttribute('data-project-index', i);
+          taskIcon.setAttribute('data-task-index', j);
+  
+          taskText.classList.add('task-text');
+          taskText.textContent = projects.projectsList[i].tasks[j].title;
+          taskText.setAttribute('data-project-index', i);
+          taskText.setAttribute('data-task-index', j);
+  
+          // Task Info Div 
+          taskInfo.classList.add('flex');
+  
+          // Tasks Due date
+          taskDate.classList.add('due-date', 'padding-right');
+          if (projects.projectsList[i].tasks[j].date !== undefined) {
+            taskDate.textContent = projects.projectsList[i].tasks[j].date;
+          } else {
+            taskDate.textContent = '';
+          }
+  
+          // Task Edit icon
+          taskEditIcon.classList.add(
+            'fal',
+            'fa-edit',
+            'edit-task',
+            'task-icon',
+            'scale-element',
+            'padding-right'
+          );
+          taskEditIcon.setAttribute('data-project-index', i);
+          taskEditIcon.setAttribute('data-task-index', j);
+  
+          // Task Delete icon
+          taskTrashIcon.classList.add(
+            'fal',
+            'fa-trash-alt',
+            'delete-task',
+            'task-icon',
+            'scale-element',
+            'padding-right'
+          );
+          taskTrashIcon.setAttribute('data-project-index', i);
+          taskTrashIcon.setAttribute('data-task-index', j);
+  
+          // Task Info icon 
+          taskInfoIcon.classList.add(
+            'fal',
+            'task-icon',
+            'scale-element',
+            'fa-info-circle'
+          );
+          taskInfoIcon.setAttribute('data-project-index', i);
+          taskInfoIcon.setAttribute('data-task-index', j);
+  
+          // Appends
+          taskIconAndTextDiv.appendChild(taskIcon);
+          taskIconAndTextDiv.appendChild(taskText);
+          taskInfo.appendChild(taskDate);
+          taskInfo.appendChild(taskEditIcon);
+          taskInfo.appendChild(taskTrashIcon);
+          taskInfo.appendChild(taskInfoIcon);
+          taskDiv.appendChild(taskIconAndTextDiv);
+          taskDiv.appendChild(taskInfo);
+          tasksList.appendChild(taskDiv);
+  
+          // Task Completion
+          if (projects.projectsList[i].tasks[j].completed === false) {
+            taskText.classList.remove('task-done-text');
+            taskIcon.classList.add(
+              'fal',
+              'fa-circle',
+              'padding-right'
+            );
+          } else {
+            taskText.classList.add('task-done-text');
+            taskIcon.classList.add(
+              'fal',
+              'fa-check-circle',
+              'padding-right'
+            );
+          }
+        }
+      }
+      manipulateModal('close');
     }
 
 
@@ -264,6 +416,7 @@ export const dom = (() => {
         showMainTitle,
         changeMainTitle,
         manipulateModal,
+        showTasks,
       };
 
 })();
