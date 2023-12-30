ff0a6ca2b7dd0a8138ed0f534b18057dddef6933
root <root@FRTOU-LT-1384197.localdomain>
2023-12-30 23:49:28 +0100

    fixing bugs with menu-link-icon class

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import handlers from './handlers';
 // When page is loaded - Show title from menu link "All" 
 dom.showMainTitle(0);
 
+
+
 // When page is loaded - Show all default projects
 dom.showProjects();
 
 // When page is loaded - Show all tasks from all default projects 
-dom.getTasks('all');
+ dom.getTasks('all');
 
 dom.responsiveMenu();
 handlers.resizeWindow();
