a29f99b95f1f85f6158664767b65a2d016582e92
root <root@FRTOU-LT-1384197.localdomain>
2023-12-30 23:35:18 +0100

    setting up index.js file in order to display projects on the page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import {dom} from './dom-manip';
+import handlers from './handlers';
 
+// When page is loaded - Show title from menu link "All" 
+dom.showMainTitle(0);
 
-// Call blanckProjectLoad on first land
+// When page is loaded - Show all default projects
+dom.showProjects();
+
+// When page is loaded - Show all tasks from all default projects 
+dom.getTasks('all');
+
+dom.responsiveMenu();
+handlers.resizeWindow();
+handlers.listenClicks();
+
 
 
 
